9ac816e34cd517a3c0a453d03356b0090177ce06
n-simone <nsimone@purdue.edu>
2015-04-14 21:00:40 -0400

    tweaked gallery again

--- assets/lightbox/lightbox_init.js
+++ assets/lightbox/lightbox_init.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("div.gallery").addClass("row");
     $("div.gallery div").each(function () {
-            $(this).replaceWith("<div style=\"height: 215px; width: 200px; overflow: hidden;\" class=\"col-sm-6 col-md-4 col-lg-3\"><a href=\"./assets/images/" + $(this).text() +"\" class=\"lightbox_trigger\"><img height=\"200\" src=\"./assets/images/" + $(this).text() + "\"/></a></div>");
+            $(this).replaceWith("<div style=\"height: 307px; width: 292px; overflow: hidden;\" class=\"col-sm-6 col-md-4 col-lg-3\"><a href=\"./assets/images/" + $(this).text() +"\" class=\"lightbox_trigger\"><img height=\"292\" src=\"./assets/images/" + $(this).text() + "\"/></a></div>");
         });
 });
